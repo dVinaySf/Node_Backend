88db9d21abc5339bc48e3743e15fd9449e53abf0
Vinay <doondi.vinay28@gmail.com>
2026-04-20 23:37:38 +0530

    Initial commit

---
+++ config/dbConn.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+console.log("DATABASE_URI:", process.env.DATABASE_URI);
+
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DATABASE_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        });
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+module.exports = connectDB
